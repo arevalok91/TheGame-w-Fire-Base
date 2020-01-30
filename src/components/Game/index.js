e5f384159b5abc6ec10d114eb8daddaf0083fe21
Nicholas Santiago <santiago.nicholas.a@gmail.com>
2020-01-29 21:02:36 -0500

    trying to add endGame and trying to add Win to Lose percentage

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -22,7 +22,10 @@ class Game extends React.Component {
         playerName: "Player1",
         score: 0,
         highScore: 0,
-        streak: 0
+        streak: 0,
+        wins: 0,
+        loses: 0,
+        percentage: 0
       },
       botPlayer:  {
         playerName: "CPU",
@@ -32,6 +35,34 @@ class Game extends React.Component {
       }
     }
   }
+  // win, lose percentage record
+   WinLosePercentage = () => {
+    let wins = this.state.player1.wins
+    let loses = this.state.player1.loses
+    let percent = this.state.player1.percentage
+    percent = wins / loses * 100
+    console.log(percent)
+    return percent
+  }
+////////////////////////////////////////////////////////
+// trying to end the game
+  EndGame = () => {
+    // let player1 = this.state.player1
+    let playerScore = this.state.player1.score
+    let cpuScore = this.state.botPlayer.score
+
+    if(playerScore === 10) {
+      return(
+        <p>Player 1 Wins</p>    //////// im still not sure if this is correct
+      )
+    }
+    if(cpuScore === 10) {
+      return(
+        <p>Cpu Wins</p>
+      )
+    }
+  }
+ ////////////////////////////////////////////////////// 
   
   // Game rules
   gameRules = (choice) => {
@@ -44,11 +75,13 @@ class Game extends React.Component {
     let winner = ""
     let gameMessage = ""
     let streak = this.state.player1.streak
+    let winLosePct = this.state.WinLosePercentage       // its undefined
     console.log('does it work?', streak)
     console.log('show me', cpuPlayer)
+    console.log('percent', winLosePct)
     botPlayer.choice = cpuPlayer
     this.setState({botPlayer: botPlayer})
-    
+    // game algorithm
     if(choice === cpuPlayer) {
       streak = 0
       gameMessage =  'tie game';
@@ -90,8 +123,12 @@ class Game extends React.Component {
       gameMessage = ""                              // need to upload list of messages
       winner = ""                                   // say something slick
     }
-      console.log('check this', player1)
 
+   
+
+      console.log(this.state.percent)
+      console.log('check this', player1)
+      
       this.setState({
         botPlayer, player1,
         gameMessage: gameMessage, winner,
